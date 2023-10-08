92f484ece3338f42468923f7186a0902d90c97f5
Asad-Anjum <anjumasad967@gmail.com>
2023-10-08 16:34:56 -0400

    Inventory System

---
+++ Assets/Sprites/Abilities/Items/InventoryManager.cs
@@ -0,0 +1,44 @@
+ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryManager : MonoBehaviour
+{
+    public static InventoryManager Instance;
+    public List<Item> Items = new List<Item>();
+
+    public Transform ItemContent;
+    public GameObject InventoryItem;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void Add(Item item)
+    {
+        Items.Add(item);
+        ListItems();
+    }
+
+    public void Remove(Item item)
+    {
+        Items.Remove(item);
+    }
+
+    public void ListItems()
+    {
+        foreach (Transform item in ItemContent)
+        {
+            Destroy(item.gameObject);
+        }
+        for(int i = Items.Count - 1; i >= 0; i--)
+        {
+            GameObject obj = Instantiate(InventoryItem, ItemContent);
+            var itemIcon = obj.transform.Find("Image").GetComponent<Image>();
+
+            itemIcon.sprite = Items[i].icon;
+        }
+    }
+}
