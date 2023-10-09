dcffa762ddd2522b0e869978a93324bca8c6d484
Roger Li <rel7817@nyu.edu>
2023-10-09 03:42:40 -0400

    boss and easy enemy update

--- Assets/Scripts/BossAttackScript.cs
+++ Assets/Scripts/BossAttackScript.cs
@@ -24,32 +24,34 @@ public class BossAttackScript : MonoBehaviour
     public AudioSource missedSFX;
     public AudioSource warningSFX;
     public float warningSFXDistance = 7f;
-    // public float voicelineCooldown = 7f;
-    // private float voicelineCDCountdown = 0f;
+    public float voicelineCooldown = 7f;
+    private float voicelineCDCountdown = 0f;
+    public bool playWarningSFX = false;
 
-    // Update is called once per frame
+    private bool alreadyHandledVoicelines = false;
     void Update()
     {
         if (duration >= attackDuration)
         {
             attacking = false;
+            alreadyHandledVoicelines = false;
             duration = 0;
         }
 
-        // if (voicelineCDCountdown <= 0 && Vector2.Distance(transform.position, playerTrans.position) <= warningSFXDistance) {
-            // warningSFX.Play();
-            // voicelineCDCountdown = voicelineCooldown;
-        // }
+        if (voicelineCDCountdown <= 0 && Vector2.Distance(transform.position, playerTrans.position) <= warningSFXDistance && playWarningSFX) {
+            warningSFX.Play();
+            voicelineCDCountdown = voicelineCooldown;
+        }
 
         anim.SetBool("Attack", false);
         if (timeBtwAttack <= 0)
         {
-            //if (Input.GetMouseButton(0))
             if (Vector2.Distance(transform.position, playerTrans.position) <= attackTriggerDistance)
             {
                 anim.SetBool("Attack", true);
                 stabSFX.Play();
                 attacking = true;
+                alreadyHandledVoicelines = false;
                 timeBtwAttack = startTimeBtwAttack;
             }
         }
@@ -62,16 +64,29 @@ public class BossAttackScript : MonoBehaviour
         {
             Collider2D[] enemiesToDamage = Physics2D.OverlapBoxAll(attackPos.position, new Vector2(RangeX, RangeY), transform.eulerAngles.z, enemies);
 
+            bool hitPlayer = false;
+
             for (int i = 0; i < enemiesToDamage.Length; i++)
             {
                 if (enemiesToDamage[i].gameObject.tag == "Player")
                 {
                     // WHAT should happen to player?;
+                    Debug.Log("ENEMY HIT PLAYER!!!");
+                    hitPlayer = true;
                 }
+            }
+
+            if (!alreadyHandledVoicelines) {
+                if (hitPlayer) {
+                    hitSFX.Play();
+                } else {
+                    missedSFX.Play();
+                }
+                alreadyHandledVoicelines = true;
             }
             duration -= Time.deltaTime;
         }
-        // voicelineCDCountdown -= Time.deltaTime;
+        voicelineCDCountdown -= Time.deltaTime;
     }
 
 
