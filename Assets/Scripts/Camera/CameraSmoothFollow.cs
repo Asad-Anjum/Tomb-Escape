0c994f327af9039e01e574438c228b9ed6e03663
小小的大肥 <11911839@mail.sustech.edu.cn>
2023-10-11 00:47:53 +0800

    enemy room (doesn't works well)

---
+++ Assets/Scripts/Camera/CameraSmoothFollow.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+
+public class CameraSmoothFollow : MonoBehaviour
+{
+    public float distance = 5.0f;
+    public float height = 1.0f;
+    public float width = 1.0f;
+    public float heightDamping = 3.0f;
+    public float widthDamping = 2.0f;
+    public float rotationDamping = 3.0f;
+    Transform _selfTransform;
+    public Transform targetObject;
+
+    void Start()
+    {
+        _selfTransform = GetComponent<Transform>();
+    }
+
+    void LateUpdate()
+    {
+        if (!targetObject) return;
+        float wantedRotationAngle = targetObject.eulerAngles.y;
+        float wantedHeight = targetObject.position.y + height;
+        float wantedWidth = targetObject.position.x + width;
+        float currentRotationAngle = _selfTransform.eulerAngles.y;
+        float currentHeight = _selfTransform.position.y;
+        float currentWidth = _selfTransform.position.x;
+        currentRotationAngle =
+            Mathf.LerpAngle(currentRotationAngle, wantedRotationAngle, rotationDamping * Time.deltaTime);
+        currentHeight = Mathf.Lerp(currentHeight, wantedHeight, heightDamping * Time.deltaTime);
+        currentWidth = Mathf.Lerp(currentWidth, wantedWidth, widthDamping * Time.deltaTime);
+        Quaternion currentRotation = Quaternion.Euler(0, currentRotationAngle, 0);
+        _selfTransform.position = targetObject.position;
+        _selfTransform.position -= currentRotation * Vector3.forward * distance;
+        Vector3 currentPosition = transform.position;
+        currentPosition.y = currentHeight;
+        currentPosition.x = currentWidth;
+        _selfTransform.position = currentPosition;
+    }
+}
