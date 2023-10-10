0935e1ac7b22b3f7c0b222cb409b0e86b1d43503
bl3649 <145500329+bl3649@users.noreply.github.com>
2023-10-10 11:47:07 -0400

    Add Title and Win screens to Assets/Scenes

---
+++ Assets/ButtonFunctions.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonFunctions : MonoBehaviour
+{
+    public void LoadTutorial()
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
+
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene("PCGTest");
+    }
+}
