85a1d9e6b1da339440d0691597ebca36ea6d558f
小小的大肥 <11911839@mail.sustech.edu.cn>
2023-10-10 23:58:04 +0800

    lever generation canvas to handling slow generation problem

--- Assets/Scripts/GameManager/LevelManager.cs
+++ Assets/Scripts/GameManager/LevelManager.cs
@@ -1,15 +1,17 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
+using System.Diagnostics;
 using Edgar.Unity;
 using UnityEngine;
+using UnityEngine.UI;
+using Debug = UnityEngine.Debug;
 
 namespace GameManager
 {
     public class LevelManager : MonoBehaviour
     {
+        public GameObject loadingCanvas;
         private DungeonGeneratorGrid2D _generator;
         private LevelInfoGrid2D _levelInfo;
-        public GameObject player;
 
         private void Awake()
         {
@@ -18,26 +20,102 @@ namespace GameManager
 
         private void Start()
         {
+            LoadLevel();
+        }
+
+
+        void LoadLevel()
+        {
+            // Show loading screen
+            ShowLoadingScreen("Dungeon", "loading..");
             StartCoroutine(GenerateLevel());
         }
 
 
         private IEnumerator GenerateLevel()
         {
-            // Start the generator coroutine
-            _generator.Generate();
+            yield return new WaitForSeconds(0.1f);
 
-            // change layername of walls... Why does this have to be a pain?
-            GameObject generatedLevel = GameObject.Find("Generated Level");
-            GameObject wallsGameObject = generatedLevel.transform.Find("Tilemaps").transform.Find("Walls").gameObject;
-            wallsGameObject.layer = LayerMask.NameToLayer("Walls");
-            Debug.Log("walls layer applied");
+            bool success = true;
+            try
+            {
+                _generator.Generate();
+            }
+            catch (TimeoutException)
+            {
+                Destroy(GameObject.Find("Generated Level"));
+                success = false;
+            }
+            finally
+            {
+                if (!success)
+                {
+                    StartCoroutine(GenerateLevel());
+                }
+            }
 
-            // start calculation A* WHY IS IT NOT SCANNING>??????
-            //AstarPath.active.Scan();
-            //Debug.Log("scanned...");
 
-            yield break;
+            if (success)
+            {
+                // change layername of walls... Why does this have to be a pain?
+                GameObject generatedLevel = GameObject.Find("Generated Level");
+                GameObject wallsGameObject =
+                    generatedLevel.transform.Find("Tilemaps").transform.Find("Walls").gameObject;
+                wallsGameObject.layer = LayerMask.NameToLayer("Walls");
+                Debug.Log("walls layer applied");
+
+                // start calculation A* WHY IS IT NOT SCANNING>??????
+                //AstarPath.active.Scan();
+                //Debug.Log("scanned...");
+
+                yield return null;
+                HideLoadingScreen();
+            }
+        }
+
+
+        /// <summary>
+        /// Hide loading screen.
+        /// </summary>
+        protected void HideLoadingScreen()
+        {
+            var canvas = loadingCanvas;
+            canvas.SetActive(false);
+            var loadingImage = canvas.transform.Find("LoadingImage")?.gameObject;
+
+            if (loadingImage != null)
+            {
+                loadingImage.SetActive(false);
+            }
+        }
+
+        /// <summary>
+        /// Show loading screen with primary and secondary text.
+        /// </summary>
+        /// <param name="primaryText"></param>
+        /// <param name="secondaryText"></param>
+        protected void ShowLoadingScreen(string primaryText, string secondaryText)
+        {
+            var canvas = loadingCanvas;
+            canvas.SetActive(true);
+            var loadingImage = canvas.transform.Find("LoadingImage")?.gameObject;
+            var primaryTextComponent = loadingImage?.transform.Find("PrimaryText")?.gameObject.GetComponent<Text>();
+            var secondaryTextComponent = loadingImage?.transform.Find("SecondaryText")?.gameObject.GetComponent<Text>();
+
+            if (loadingImage != null)
+            {
+                loadingImage.SetActive(true);
+            }
+
+            if (primaryTextComponent != null)
+            {
+                primaryTextComponent.text = primaryText;
+            }
+
+            if (secondaryTextComponent != null)
+            {
+                secondaryTextComponent.text = secondaryText;
+            }
         }
     }
 }
